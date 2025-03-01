511354d8b22e9af15490f40a29d137c787612c49
Gabriel Silva <contato@gabrieljs.dev>
2025-03-01 17:43:31 -0300

    fix blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,35 +1,24 @@
 "use client";
 import { Footer } from "@/components/sections/footer.section";
 import { Navbar } from "@/components/sections/navbar";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import {
-	Card,
-	CardContent,
-	CardFooter,
-	CardHeader,
-} from "@/components/ui/card";
 import { useTranslations } from "@/constants/profile";
-import { useLanguage } from "@/contexts/language-context";
-import { formatDistanceToNow } from "date-fns";
-import { Calendar, Clock, Eye } from "lucide-react";
-import Link from "next/link";
 
 export default function BlogPage() {
-	function calculateReadingTime(content: string): number {
-		const wordsPerMinute = 200;
-		const wordCount = content.split(/\s+/).length;
-		return Math.ceil(wordCount / wordsPerMinute);
-	}
+	// function calculateReadingTime(content: string): number {
+	// 	const wordsPerMinute = 200;
+	// 	const wordCount = content.split(/\s+/).length;
+	// 	return Math.ceil(wordCount / wordsPerMinute);
+	// }
+	//
+	// function truncateContent(content: string, wordLimit: number): string {
+	// 	const words = content.split(/\s+/);
+	// 	if (words.length <= wordLimit) return content;
+	// 	return `${words.slice(0, wordLimit).join(" ")}...`;
+	// }
+	//
+	// const { dateFnsLocale } = useLanguage();
 
-	function truncateContent(content: string, wordLimit: number): string {
-		const words = content.split(/\s+/);
-		if (words.length <= wordLimit) return content;
-		return `${words.slice(0, wordLimit).join(" ")}...`;
-	}
-
-	const { dateFnsLocale } = useLanguage();
-	const { profile, ui } = useTranslations();
+	const { ui } = useTranslations();
 
 	return (
 		<>
@@ -42,67 +31,67 @@ export default function BlogPage() {
 
 					<h2 className={"text-2xl font-bold"}>{ui.comingSoon}</h2>
 
-					<div className="grid gap-6">
-						{profile.posts.map((post) => (
-							<Card
-								key={post.id}
-								className="flex flex-col transition-all hover:shadow-lg dark:hover:shadow-primary/10"
-							>
-								<CardHeader>
-									<h2 className="text-2xl font-semibold leading-tight text-foreground">
-										{post.title}
-									</h2>
-								</CardHeader>
-								<CardContent>
-									<p className="text-muted-foreground">
-										{truncateContent(post.content, 50)}
-									</p>
+					{/*<div className="grid gap-6">*/}
+					{/*	{profile.posts.map((post) => (*/}
+					{/*		<Card*/}
+					{/*			key={post.id}*/}
+					{/*			className="flex flex-col transition-all hover:shadow-lg dark:hover:shadow-primary/10"*/}
+					{/*		>*/}
+					{/*			<CardHeader>*/}
+					{/*				<h2 className="text-2xl font-semibold leading-tight text-foreground">*/}
+					{/*					{post.title}*/}
+					{/*				</h2>*/}
+					{/*			</CardHeader>*/}
+					{/*			<CardContent>*/}
+					{/*				<p className="text-muted-foreground">*/}
+					{/*					{truncateContent(post.content, 50)}*/}
+					{/*				</p>*/}
 
-									<Link href={`/post/${post.id}`}>
-										<Button className={"mx-0 px-0"} variant="link">
-											{ui.viewMore}
-										</Button>
-									</Link>
-								</CardContent>
-								<CardFooter className="mt-auto flex flex-col items-start gap-4">
-									<div className="flex flex-wrap items-center gap-4 text-sm text-muted-foreground">
-										<div className="flex items-center gap-1.5">
-											<Calendar className="h-4 w-4" />
-											<time dateTime={post.date.toISOString()}>
-												{formatDistanceToNow(post.date, {
-													addSuffix: true,
-													locale: dateFnsLocale,
-												})}
-											</time>
-										</div>
-										<div className="flex items-center gap-1.5">
-											<Clock className="h-4 w-4" />
-											<span>
-												{calculateReadingTime(post.content)} {ui.minRead}
-											</span>
-										</div>
-										<div className="flex items-center gap-1.5">
-											<Eye className="h-4 w-4" />
-											<span>
-												{post.views} {ui.views}
-											</span>
-										</div>
-									</div>
-									<div className="flex flex-wrap gap-2 pt-4">
-										{post.tags.map((tag) => (
-											<Badge
-												key={tag}
-												variant="secondary"
-												className="text-xs font-medium hover:bg-secondary/80"
-											>
-												#{tag.toLowerCase().replace(/\s+/g, "")}
-											</Badge>
-										))}
-									</div>
-								</CardFooter>
-							</Card>
-						))}
-					</div>
+					{/*				<Link href={`/post/${post.id}`}>*/}
+					{/*					<Button className={"mx-0 px-0"} variant="link">*/}
+					{/*						{ui.viewMore}*/}
+					{/*					</Button>*/}
+					{/*				</Link>*/}
+					{/*			</CardContent>*/}
+					{/*			<CardFooter className="mt-auto flex flex-col items-start gap-4">*/}
+					{/*				<div className="flex flex-wrap items-center gap-4 text-sm text-muted-foreground">*/}
+					{/*					<div className="flex items-center gap-1.5">*/}
+					{/*						<Calendar className="h-4 w-4" />*/}
+					{/*						<time dateTime={post.date.toISOString()}>*/}
+					{/*							{formatDistanceToNow(post.date, {*/}
+					{/*								addSuffix: true,*/}
+					{/*								locale: dateFnsLocale,*/}
+					{/*							})}*/}
+					{/*						</time>*/}
+					{/*					</div>*/}
+					{/*					<div className="flex items-center gap-1.5">*/}
+					{/*						<Clock className="h-4 w-4" />*/}
+					{/*						<span>*/}
+					{/*							{calculateReadingTime(post.content)} {ui.minRead}*/}
+					{/*						</span>*/}
+					{/*					</div>*/}
+					{/*					<div className="flex items-center gap-1.5">*/}
+					{/*						<Eye className="h-4 w-4" />*/}
+					{/*						<span>*/}
+					{/*							{post.views} {ui.views}*/}
+					{/*						</span>*/}
+					{/*					</div>*/}
+					{/*				</div>*/}
+					{/*				<div className="flex flex-wrap gap-2 pt-4">*/}
+					{/*					{post.tags.map((tag) => (*/}
+					{/*						<Badge*/}
+					{/*							key={tag}*/}
+					{/*							variant="secondary"*/}
+					{/*							className="text-xs font-medium hover:bg-secondary/80"*/}
+					{/*						>*/}
+					{/*							#{tag.toLowerCase().replace(/\s+/g, "")}*/}
+					{/*						</Badge>*/}
+					{/*					))}*/}
+					{/*				</div>*/}
+					{/*			</CardFooter>*/}
+					{/*		</Card>*/}
+					{/*	))}*/}
+					{/*</div>*/}
 				</div>
 			</div>
 			<Footer />
